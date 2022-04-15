7f7aecbb99770b4d7a5167afebe256aeef5c4887
Eniyavan <eniyavan133@gmail.com>
2022-04-15 07:45:35 +0530

    last update

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const app=express();
-const  cors = require("cors");
-app.use(cors());
-const url='mongodb://127.0.0.1:27017/employesdb'
-let db;
- mongoose.connect(url,{useNewUrlParser:true, useUnifiedTopology: true}).then(x => {
-     console.log("mongodb connected");
-      db=mongoose.connection
-    })
-app.use(express.json({limit:'200mb'}));
-app.use(express.urlencoded({limit:'200mb',extended:true}));
-app.use(express.text({limit:'200mb'}))
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const cors = require("cors");
 
-const dataRouter=require('./routers/data')
-app.use('/data',dataRouter);
-app.listen(5000,()=>{console.log("connected")});
+app.use(cors());
+const url = 'mongodb://127.0.0.1:27017/employesdb'
+let db;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }).then(x => {
+  console.log("mongodb connected");
+  db = mongoose.connection
+})
+
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ limit: '200mb', extended: true }));
+app.use(express.text({ limit: '200mb' }))
+
+const dataRouter = require('./routers/data')
+app.use('/data', dataRouter);
+app.listen(5000, () => { console.log("connected") });
--- modules/dataone.js
+++ modules/dataone.js
@@ -1,28 +1,28 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
 const dataSchema = new mongoose.Schema({
-    name:{
-        type:String, 
-},
+    name: {
+        type: String,
+    },
 
-  mobile:{
-      type:String,
-  },
+    mobile: {
+        type: String,
+    },
 
-  email:{
-      type:String,
-  },
-  dob:{
-      type:String,
-  },
-  jobtype:{
-      type:String,
-  },
-  location:{
-      type:String
-  },
-  pic:{
-      type:String
-  }
+    email: {
+        type: String,
+    },
+    dob: {
+        type: String,
+    },
+    jobtype: {
+        type: String,
+    },
+    location: {
+        type: String
+    },
+    pic: {
+        type: String
+    }
 })
-module.exports = mongoose.model('DataOne',dataSchema, "employees");
+module.exports = mongoose.model('DataOne', dataSchema, "employees");
